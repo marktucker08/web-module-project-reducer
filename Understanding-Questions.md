61993b4e25510baf6565d93a6c49a00b9ac0f25a
Mark Tucker <markdaddy@Marks-MacBook-Air.local>
2022-12-08 20:35:55 -0700

    completed addOne event handler case

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,9 @@
 # Understanding Questions:
 1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
 * The user presses the 1 button.
-* 
+* event handler anonymous function passes the addOne action creator to the CalcButton component
+* addOne returns the ADD_ONE case to send to the reducer function.
+* the reducer sets the new total as total + 1
 ...
 
 * TotalDisplay shows the total plus 1.
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useReducer } from 'react';
 import reducer, { initialState } from '../reducers';
+import { addOne } from '../actions';
 
 import './App.css';
 
@@ -8,6 +9,10 @@ import CalcButton from './CalcButton';
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  const handle1Click = () => {
+    dispatch(addOne())
+  }
 
   return (
     <div className="App">
@@ -32,7 +37,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={() => dispatch(addOne())}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
